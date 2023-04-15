3b7799b9e18263489b557b7e89548146cee0bae5
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-04-15 19:08:14 +0900

    refactoring level1 ver3

--- week03/Javascript/level1/refactoring/level.js
+++ week03/Javascript/level1/refactoring/level.js
@@ -1,22 +1,24 @@
 import { BANK_LIST, ACCOUNT_FORM } from "./account.js";
 
+// 배열 초기화
 const BANK_LIST_ARRAY = Object.values(BANK_LIST);
 const ACCOUNT_FORM_ARRAY = Object.values(ACCOUNT_FORM);
 
+// DOM 엘리먼트 선택
 const $select = document.querySelector("#bank-selector");
 const $send_form = document.querySelector("#account-send-form");
 const $list = document.querySelector("#account-list");
 const $account_input = document.querySelector("#account-input");
 let bank_list = [];
 
+// 페이지가 로드될 때 은행 선택 목록을 업데이트합니다.
 window.onload = () => {
   $select.innerHTML = BANK_LIST_ARRAY.map((item) => {
-    {
-      return `<option value="${item}">${item}</option>`;
-    }
+    return `<option value="${item}">${item}</option>`;
   }).join("");
 };
 
+// 양식을 제출할 때 새 계좌를 목록에 추가하고 목록을 업데이트합니다.
 $send_form.addEventListener("submit", (e) => {
   e.preventDefault();
   const bankValue = $select.value;
@@ -26,14 +28,7 @@ $send_form.addEventListener("submit", (e) => {
   $account_input.value = "";
 });
 
-const listUpdate = () => {
-  $list.innerHTML = bank_list
-    .map((list) => {
-      return `<li>${list[0]} : ${list[1]}</li>`;
-    })
-    .join("");
-};
-
+// 새 계좌를 목록에 추가하는 함수입니다.
 const accountListAdded = (List, Bank, Account, checkNumber) => {
   if (checkStringLength(Account, checkNumber)) {
     const newAccount = changeNewAccountNumber(
@@ -47,12 +42,19 @@ const accountListAdded = (List, Bank, Account, checkNumber) => {
   }
 };
 
-/**
- *
- * @param {Array} changeCharinListindex
- * @param {String} prevString
- * @returns String
- */
+// 목록을 업데이트하는 함수입니다.
+const listUpdate = () => {
+  $list.innerHTML = bank_list
+    .map((list) => {
+      return `
+        <li>
+          ${list[0]} : ${list[1]}
+        </li>`;
+    })
+    .join("");
+};
+
+// 새 계좌 번호를 변경하는 함수입니다.
 function changeNewAccountNumber(changeCharinListindex, prevString, dontChangeRange) {
   const newString = prevString
     .split("")
@@ -71,34 +73,18 @@ function changeNewAccountNumber(changeCharinListindex, prevString, dontChangeRan
   return newString;
 }
 
-/**
- *
- * @param {String} checkString
- * @param {Number} count
- * @returns boolean
- */
+// 문자열 길이를 확인하는 함수입니다.
 function checkStringLength(checkString, count) {
   return checkString.length === count;
 }
 
-/**
- *
- * @param {Array} KeyList
- * @param {String} FindKey
- * @param {Array} ValueList
- * @returns FindKey width ValuList
- */
+// 배열에서 특정 키의 값(value)을 찾아 반환하는 함수입니다.
 function getValueWithArrayIndex(KeyList, FindKey, ValueList) {
   const result = ValueList[KeyList.indexOf(FindKey)];
   return result;
 }
 
-/**
- *
- * @param {String} String
- * @param {Char} Char
- * @returns Findindex(char) in Array
- */
+// 문자열에서 모든 인덱스(index)를 찾아 배열로 반환하는 함수입니다.
 function findIndexAllinString(String, Char) {
   const result = String.split("").reduce((accumulator, current, index) => {
     if (current === Char) {
